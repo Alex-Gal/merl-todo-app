51f1a23fb0af4893cc17a55cdb4c58e2d210f807
Alexey Galashko <82465083+Alex-Gal@users.noreply.github.com>
2022-03-28 17:28:26 +0300

    make registration

--- client/src/pages/AuthPage/Registration/Registration.jsx
+++ client/src/pages/AuthPage/Registration/Registration.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../AuthPage.scss'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export const Registration = () => {
+
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+
+    const changeHandler = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+        console.log(e.target.name);
+    }
+
+    const registerHandler = async () => {
+        try {
+            await axios.post('/api/auth/registration', {...form}, {
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+            .then(response => console.log(response))
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className='container'>
             <div className='auth-page'>
                 <h3>Регистрация</h3>
-                <form className="form form-login">
+                <form className="form form-login" onSubmit={e => e.preventDefault()}>
                     <div className='row'>
                         <div className='input-field col s12'>
                             <input
                                 type="email"
                                 name="email"
                                 className='validate'
+                                onChange={changeHandler}
                             />
                             <label htmlFor="email">Email</label>
                         </div>
@@ -22,6 +48,7 @@ export const Registration = () => {
                                 type="password"
                                 name="password"
                                 className='validate'
+                                onChange={changeHandler}
                             />
                             <label htmlFor="password">Password</label>
                         </div>
@@ -29,10 +56,11 @@ export const Registration = () => {
                     <div className='row'>
                         <button
                             className='wawes-effect wawes-light btn blue'
+                            onClick={registerHandler}
                         >
                             Регистрация
                         </button>
-                        <Link to="/" className="btn-outline btn-reg">Аккаунт уже существует</Link>
+                        <Link to="/login" className="btn-outline btn-reg">Аккаунт уже существует</Link>
                     </div>
                 </form>
             </div>
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(express.json({extended: true}))
+app.use('/api/auth', require('./routes/auth.route'))
+
 async function start() {
     try {
         await mongoose.connect('mongodb+srv://Alex-Gal:Lexa2084163@cluster0.ocia8.mongodb.net/merl-todo-app?retryWrites=true&w=majority')
