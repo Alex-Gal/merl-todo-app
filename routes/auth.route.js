a1b7b190a04e67d00a3b91836f8df83b2ce5e8b2
Alexey Galashko <82465083+Alex-Gal@users.noreply.github.com>
2022-04-05 17:30:01 +0300

    add hasher and validator

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,27 +1,45 @@
 const {Router} = require('express')
 const router = Router()
 const User = require('../models/User')
+const {check, validationResult} = require('express-validator')
+const bcrypt = require('bcryptjs')
 
-router.post('/registration', async (req, res) => {
-    try {
-        const { email, password } = req.body
+router.post('/registration', 
+    [
+        check('email', 'Некорректный email').isEmail(),
+        check('password', 'Некорректный пароль').isLength({min: 6})
+    ],
+    async (req, res) => {
+        try {
+            
+            const errors = validationResult(req)
+            if(errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Некорректные данные при регистрации'
+                })
+            }
 
-        const isUsed = await User.findOne({ email })
+            const { email, password } = req.body
 
-        if(isUsed) {
-            return res.status(300).json({message: 'Данный email занят, попробуйте другой.'})
+            const isUsed = await User.findOne({ email })
+
+            if(isUsed) {
+                return res.status(300).json({message: 'Данный email занят, попробуйте другой.'})
+            }
+            
+            const hashedPassword = await bcrypt.hash(password, 12)
+
+            const user = new User({
+                email, password: hashedPassword
+            })
+
+            await user.save()
+            res.status(201).json({message: 'Пользователь создан'})
+
+        } catch (error) {
+            console.log('Error', error);
         }
-
-        const user = new User({
-            email, password
-        })
-
-        await user.save()
-        res.status(201).json({message: 'Пользователь создан'})
-
-    } catch (error) {
-        console.log('Error', error);
-    }
 })
 
 module.exports = router
